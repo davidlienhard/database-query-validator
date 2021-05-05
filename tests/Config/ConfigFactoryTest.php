b8aff1dc171c5f9f6558d8820dc1cd3eaf402f34
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2021-05-05 06:42:00 +0200

    add test for config factory (#61)
    add new unit test for config factory

---
+++ tests/Config/ConfigFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DavidLienhard\Database\QueryValidator\Tests\DumpData;
+
+use DavidLienhard\Database\QueryValidator\Config\Config;
+use DavidLienhard\Database\QueryValidator\Config\ConfigInterface;
+use DavidLienhard\Database\QueryValidator\Config\Factory as ConfigFactory;
+use League\Flysystem\Filesystem;
+use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
+use PHPUnit\Framework\TestCase;
+
+class ConfigFactoryTestCase extends TestCase
+{
+    private function getFilesystem() : Filesystem
+    {
+        $adapter = new InMemoryFilesystemAdapter;
+        return new Filesystem($adapter);
+    }
+
+    /**
+     * @covers DavidLienhard\Database\QueryValidator\Config\Factory
+     * @test
+     */
+    public function testCanCreateConfigFromEmptyJsonFile(): void
+    {
+        $filesystem = $this->getFilesystem();
+        $filesystem->write("config.json", "{}");
+
+        $config = ConfigFactory::fromJson($filesystem, "config.json");
+
+        $this->assertInstanceOf(Config::class, $config);
+        $this->assertInstanceOf(ConfigInterface::class, $config);
+    }
+
+    /**
+     * @covers DavidLienhard\Database\QueryValidator\Config\Factory
+     * @test
+     */
+    public function testGetExceptionFromInexistentJsonFile(): void
+    {
+        $filesystem = $this->getFilesystem();
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("configuration file 'config.json' does not exist");
+        ConfigFactory::fromJson($filesystem, "config.json");
+    }
+
+    /**
+     * @covers DavidLienhard\Database\QueryValidator\Config\Factory
+     * @test
+     */
+    public function testGetExceptionFromInvalidJsonFile(): void
+    {
+        $filesystem = $this->getFilesystem();
+        $filesystem->write("config.json", "{");
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("unable to decode json data");
+        ConfigFactory::fromJson($filesystem, "config.json");
+    }
+
+    /**
+     * @covers DavidLienhard\Database\QueryValidator\Config\Factory
+     * @test
+     */
+    public function testCanReadDataFromJsonFile(): void
+    {
+        $filesystem = $this->getFilesystem();
+        $filesystem->write("config.json", "{ \"key\": \"value\" }");
+
+        $config = ConfigFactory::fromJson($filesystem, "config.json");
+        $this->assertEquals("value", $config->get("key"));
+    }
+
+    /**
+     * @covers DavidLienhard\Database\QueryValidator\Config\Factory
+     * @test
+     */
+    public function testCanAddFromstdinKeyFromArgument(): void
+    {
+        $filesystem = $this->getFilesystem();
+        $filesystem->write("config.json", "{}");
+
+
+        $config = ConfigFactory::fromJson($filesystem, "config.json");
+        $this->assertEquals(null, $config->get("parameters", "fromstdin"));
+
+        $_SERVER['argv'][] = "--from-stdin";
+        $config = ConfigFactory::fromJson($filesystem, "config.json");
+        $this->assertEquals(true, $config->get("parameters", "fromstdin"));
+    }
+}

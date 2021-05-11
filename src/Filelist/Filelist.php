8604274daefc08e8811da09c4e7b42ddece7236e
David Lienhard <david.lienhard@tourasia.ch>
2021-05-11 11:25:04 +0200

    add filelist add new filelist class to store files to scan

---
+++ src/Filelist/Filelist.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * contains Filelist class
+ *
+ * @author          David Lienhard <github@lienhard.win>
+ * @copyright       David Lienhard
+ */
+
+declare(strict_types=1);
+
+namespace DavidLienhard\Database\QueryValidator\Filelist;
+
+use DavidLienhard\Database\QueryValidator\Filelist\FilelistInterface;
+
+class Filelist implements FilelistInterface
+{
+    /**
+     * creates a new filelist
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     * @param           array<\SplFileinfo>     $filelist       list of files to scan
+     */
+    public function __construct(private array $filelist = [])
+    {
+        foreach ($filelist as $file) {
+            if (!($file instanceof \SplFileinfo)) {
+                throw new \InvalidArgumentException("file must be instance of \SplFileinfo");
+            }
+        }
+    }
+
+    /**
+     * adds a new file to the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     * @param           \SplFileinfo    $file       file to add to the list
+     */
+    public function addFile(\SplFileinfo $file) : void
+    {
+        $this->filelist[] = $file;
+    }
+
+    /**
+     * returns all files in the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     */
+    public function getFiles() : array
+    {
+        return $this->filelist;
+    }
+
+    /**
+     * returns the number of files in the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     */
+    public function getFilecount() : int
+    {
+        return count($this->filelist);
+    }
+}
---
+++ src/Filelist/FilelistInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * contains Filelist interface
+ *
+ * @author          David Lienhard <github@lienhard.win>
+ * @copyright       David Lienhard
+ */
+
+declare(strict_types=1);
+
+namespace DavidLienhard\Database\QueryValidator\Filelist;
+
+interface FilelistInterface
+{
+    /**
+     * creates a new filelist
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     * @param           array<\SplFileinfo>     $filelist       list of files to scan
+     */
+    public function __construct(array $filelist = []);
+
+    /**
+     * adds a new file to the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     * @param           \SplFileinfo    $file       file to add to the list
+     */
+    public function addFile(\SplFileinfo $file) : void;
+
+    /**
+     * returns all files in the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     */
+    public function getFiles() : array;
+
+    /**
+     * returns the number of files in the list
+     *
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
+     */
+    public function getFilecount() : int;
+}

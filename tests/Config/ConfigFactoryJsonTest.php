a11e603d050fff1c51f6bebe631720475996d62a
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2021-06-03 09:00:01 +0200

    rename config factory test (#90)
    rename file to `ConfigFactoryJsonTest` to have an individual file for the json method

--- tests/Config/ConfigFactoryJsonTest.php
+++ tests/Config/ConfigFactoryJsonTest.php
@@ -12,7 +12,7 @@ use League\Flysystem\Filesystem;
 use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
 use PHPUnit\Framework\TestCase;
 
-class ConfigFactoryTestCase extends TestCase
+class ConfigFactoryJsonTestCase extends TestCase
 {
     private function getFilesystem() : Filesystem
     {

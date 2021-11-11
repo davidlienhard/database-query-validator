b8413b680be9171b8d13cb06bb37a31877df10ff
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2021-11-11 21:08:16 +0100

    make sure data returned from config is array
    check if data from config is an array or throw otherwise

--- src/Config/Factory.php
+++ src/Config/Factory.php
@@ -51,6 +51,12 @@ class Factory
             );
         }
 
+        if (!is_array($data)) {
+            throw new ConfigException(
+                "unable to decode json data"
+            );
+        }
+
         $config = self::addFromArguments($data);
 
         return $config;
@@ -67,6 +73,12 @@ class Factory
                 "unable to decode yaml data",
                 intval($e->getCode()),
                 $e
+            );
+        }
+
+        if (!is_array($data)) {
+            throw new ConfigException(
+                "unable to decode yaml data"
             );
         }
 
